3df48ae5dbe3dca0245be9edc72d3d75322a13f6
Morten Kristensen <Mortenkristensen13@gmail.com>
2020-04-08 19:13:21 +0200

    Attempt to solve error on CI.

--- DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Tests/Trees/BinaryTreeTests.cs
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Tests/Trees/BinaryTreeTests.cs
@@ -3,20 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace DataStructuresAndAlgorithms.Datastructures.Trees.Tests
-{
+namespace DataStructuresAndAlgorithms.Datastructures.Trees.Tests {
+
     [TestFixture, Category("Test")]
-    class BinaryTreeTests
-    {
+    class BinaryTreeTests {
 
         [Test]
         public void Test() {
-
             var tree = new BinaryTree<string>();
-
             tree.Add("a");
             tree.Add("b");
-
             tree.GetIndex(2).Equals("a");
             tree.GetIndex(1).Equals("b");
         }
